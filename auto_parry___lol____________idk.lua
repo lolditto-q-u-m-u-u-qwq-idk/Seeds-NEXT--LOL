5b5d8db7a20bf9988d925ac191aa2603ae77d80d
lolditto-q-u-m-u-u-qwq-idk <3552938843@qq.com>
2024-06-23 13:24:41 +0800

    Update auto_parry___lol____________idk.lua

--- auto_parry___lol____________idk.lua
+++ auto_parry___lol____________idk.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lolditto-q-u-m-u-u-qwq-idk/Seeds-NEXT--LOL/main/16336"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lolditto-q-u-m-u-u-qwq-idk/Seeds-NEXT--LOL/main/16387"))()
