37cd6653045f950c8a604996ba04b40f532acdbf
lolditto-q-u-m-u-u-qwq-idk <3552938843@qq.com>
2024-07-24 12:30:28 +0800

    Create auto_parry___lol____________idk.lua
    nothing

---
+++ auto_parry___lol____________idk.lua
@@ -0,0 +1,4 @@
+--The Script By Qumu and wevyyds16 
+--Discord Link https://discord.gg/VdCgGjvefn
+--A Cool Blade Ball Script LOL
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lolditto-q-u-m-u-u-qwq-idk/Seeds-NEXT--LOL/main/ditto.txt"))()
